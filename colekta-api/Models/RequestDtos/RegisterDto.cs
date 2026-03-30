541584955ec3f79d379487a1b40c446407076bb3
beckmanz <mauriciosantosnetto@gmail.com>
2026-03-30 18:06:03 -0300

    feat(dtos): adicionar DTOs para registro e resposta de autenticação

---
+++ colekta-api/Models/RequestDtos/RegisterDto.cs
@@ -0,0 +1,3 @@
+namespace colekta_api.Models.RequestDtos;
+
+public record RegisterDto(string NomeCompleto, string Email, string Senha);
---
+++ colekta-api/Models/ResponseDtos/AuthResponseDto.cs
@@ -0,0 +1,10 @@
+namespace colekta_api.Models.ResponseDtos;
+
+public class AuthResponseDto
+{
+    public string Id { get; set; }
+    public string NomeCompleto { get; set; }
+    public string UserName { get; set; }
+    public string cpf { get; set; }
+    public string Email { get; set; }
+}
