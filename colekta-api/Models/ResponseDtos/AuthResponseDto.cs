42103f2e8304c07cbcf42c6889d32efc760c0177
beckmanz <mauriciosantosnetto@gmail.com>
2026-04-05 20:42:28 -0300

    feat(auth): adicionar propriedades e Mapper para conversão no DTO de resposta de autenticação

--- colekta-api/Models/ResponseDtos/AuthResponseDto.cs
+++ colekta-api/Models/ResponseDtos/AuthResponseDto.cs
@@ -1,3 +1,5 @@
+using colekta_api.Models.Entities;
+
 namespace colekta_api.Models.ResponseDtos;
 
 public class AuthResponseDto
@@ -5,6 +7,19 @@ public class AuthResponseDto
     public string Id { get; set; }
     public string NomeCompleto { get; set; }
     public string UserName { get; set; }
+    public List<string>? Roles { get; set; }
     public string cpf { get; set; }
     public string Email { get; set; }
+    
+    public static AuthResponseDto ToAuthResponseDto(ApplicationUserModel user)
+    {
+        return new AuthResponseDto
+        {
+            Id = user.Id,
+            NomeCompleto = user.FullName,
+            UserName = user.UserName ?? string.Empty,
+            cpf = user.Cpf ?? string.Empty,
+            Email = user.Email ?? string.Empty
+        };
+    }
 }
