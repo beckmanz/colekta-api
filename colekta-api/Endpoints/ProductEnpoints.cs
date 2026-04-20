e6004606092f48cff12535f6bd41f76f8c179a68
beckmanz <mauriciosantosnetto@gmail.com>
2026-04-20 18:56:02 -0300

    feat(produto): adicionar endpoints para criação e listagem de produtos com autorização

--- colekta-api/Endpoints/ProductEnpoints.cs
+++ colekta-api/Endpoints/ProductEnpoints.cs
@@ -1,5 +1,9 @@
+using System.Security.Claims;
 using colekta_api.Models.FiltersDto;
+using colekta_api.Models.RequestDtos;
 using colekta_api.Services.Product;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace colekta_api.Endpoints;
 
@@ -8,16 +12,29 @@ public static class ProductEnpoints
     public static void MapProductEndpoints(this WebApplication app)
     {
         var group = app.MapGroup("/api/products")
-            .WithTags("Products");
-        
+            .WithTags("Products")
+            .RequireAuthorization("Admin")
+            .RequireAuthorization("ElevatedRights");
+
         group.MapGet("/", async (IProductInterface productService, [AsParameters] ProductFilterDto filter) =>
-        {
-            var result = await productService.GetAllProductAsync(filter);
-            return result;
-        }).WithName("Products")
-        .WithSummary("Retorna uma lista de produtos")
-        .WithDescription("Retorna uma lista de produtos com base nos filtros fornecidos. Os filtros podem incluir categoria, faixa de preço, nome, entre outros.");
-        
+            {
+                var result = await productService.GetAllProductAsync(filter);
+                return result;
+            }).WithName("Products")
+            .WithSummary("Retorna uma lista de produtos")
+            .WithDescription(
+                "Retorna uma lista de produtos com base nos filtros fornecidos. Os filtros podem incluir categoria, faixa de preço, nome, entre outros.")
+            .AllowAnonymous();
+
+        group.MapPost("/", async (IProductInterface productService, ClaimsPrincipal user, [FromForm]CreateProductDto product) =>
+            {
+                var result = await productService.CreateProductAsync(product, user);
+                return result;
+            }).DisableAntiforgery()
+            .WithName("CreateProduct")
+            .WithSummary("Cria um novo produto")
+            .WithDescription(
+                "Cria um novo produto com base nos dados fornecidos. O usuário deve ser autenticado e ter a função de 'Admin' ou 'Vendedor' para criar um produto.");
         
     }
 }
