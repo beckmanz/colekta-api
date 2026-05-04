d6babc29ec0b7873bd7a612176ef4e020c18b1aa
beckmanz <mauriciosantosnetto@gmail.com>
2026-05-03 22:45:36 -0300

    feat(categoria): adicionar endpoint para criar nova categoria

--- colekta-api/Endpoints/CategoryEndpoints.cs
+++ colekta-api/Endpoints/CategoryEndpoints.cs
@@ -1,6 +1,6 @@
 using colekta_api.Models.FiltersDto;
 using colekta_api.Services.Category;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi;
 
 namespace colekta_api.Endpoints;
 
@@ -9,14 +9,25 @@ public static class CategoryEndpoints
     public static void MapCategoryEndpoints(this IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("/api/categories")
-            .WithTags("Categories");
+            .WithTags("Categories")
+            .RequireAuthorization("ElevatedRights");
         
         group.MapGet("", async (ICategoryInterface categoryInterface, [AsParameters] CategoryFilterDto filter) =>
         {
             var result = await categoryInterface.GetAllCategories(filter);
             return result;
         }).WithName("GetAllCategories")
+        .AllowAnonymous()
         .WithSummary("Lista todas as categorias")
         .WithDescription("Retorna uma lista paginada de todas as categorias disponíveis na plataforma.");
+        
+        group.MapPost("", async (ICategoryInterface categoryInterface, string nome) =>
+        {
+            var result = await categoryInterface.CreateCategoryAsync(nome);
+            return result;
+        }).WithName("CreateCategory")
+        .WithTags("Categories")
+        .WithSummary("Cria uma nova categoria")
+        .WithDescription("Cria uma nova categoria com base no nome fornecido. O nome deve ser único e não pode estar vazio. Retorna a categoria criada ou um erro se a categoria já existir.");
     }
 }
